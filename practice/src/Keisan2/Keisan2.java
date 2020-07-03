8608b13928bac4092cdb10fc95a533938c6626c3
Miyuki <miyuki.matsumoto1011@gmail.com>
2020-07-04 01:39:53 +0900

    四則演算プログラムを追加

---
+++ practice/src/Keisan2/Keisan2.java
@@ -0,0 +1,22 @@
+package Keisan2;
+
+/*******************/
+/* 四則演算         */
+/*******************/
+
+public class Keisan2 {
+	public static void main(String[] args) {
+		int numberX = 5;
+		int numberY = 8;
+		int numberZ = 6;
+
+		int answer1 = numberX + numberY;
+		System.out.println("答えは：" + answer1);
+
+	    int answer2 = numberY - numberX;
+	    System.out.println("答えは：" + answer2);
+
+	    int answer3 = numberX * numberZ;
+	    System.out.println("答えは：" + answer3);
+	}
+}
