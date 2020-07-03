828ec3b9bcb554a1b5ff588da3b57a0dcf193cfc
Miyuki <miyuki.matsumoto1011@gmail.com>
2020-07-04 01:39:21 +0900

    Hello Worldプログラムを追加

---
+++ practice/src/Test1/Test1.java
@@ -0,0 +1,15 @@
+package Test1;
+
+/*******************/
+/* Hello World     */
+/*******************/
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		String name = "World";
+
+		System.out.println("Hello " + name);
+		System.out.println("Hello " + name + " again!");
+	}
+}
